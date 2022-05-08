33e9a78a1ba3c6c607d5e87a8f9fcb9b86a1bb7f
Juducarm <jujducrm@gmail.com>
2022-05-08 11:54:49 +0200

    NightMode change langue encore problématique

--- app/src/main/java/be/julien/info_h20_demineur/MainActivity.kt
+++ app/src/main/java/be/julien/info_h20_demineur/MainActivity.kt
@@ -33,22 +33,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var timeBarView: TimeBarView
     lateinit var appSettingPrefs: SharedPreferences
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN); //cachage de la barre de status
+        setContentView(layout.activity_main)
+        btnChangeFragment.setOnClickListener(this)
+        timeBarView = findViewById<TimeBarView>(R.id.timeBarView)
+        setFragmentMenu()
+        appSettingPrefs = getSharedPreferences("AppSettingPrefs", 0)
+        val sharedPrefEdit: SharedPreferences.Editor = appSettingPrefs.edit()
+        sharedPrefEdit.putBoolean("NightMode", false)
+
+    }
 
     override fun onClick(v: View) {
 
-
-
-        when(v.id) { //when au lieu de setOnClickListener pour pouvoir mettre plusieurs boutons si besoin
+        when(v.id) { //when au lieu de setOnClickListener pour pouvoir mettre plusieurs boutons facilement si besoin
             R.id.btnChangeFragment -> {
 
                 if (onMenu) {
                     btnChangeFragment.text = getString(string.afficher_menu)
 
-                    //transaction vers le nouveau fragment
-                    val transaction = manager.beginTransaction()
-                    transaction.replace(id.fragment_container, fragmentField)
-                    transaction.addToBackStack(null) //conserve le fragment en mémoire
-                    transaction.commit()
+                   setFragmentField()
 
                     timeBarView.background = Color.TRANSPARENT.toDrawable()
                     timeBarView.startDrawing()
@@ -57,32 +63,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 else {
                     btnChangeFragment.text = getString(string.afficher_jeu)
 
-                    //transaction vers le nouveau fragment
-                    val transaction = manager.beginTransaction()
-                    transaction.replace(id.fragment_container, fragmentMenu)
-                    transaction.addToBackStack(null) //conserve le fragment en mémoire
-                    transaction.commit()
+                    setFragmentMenu()
 
                     timeBarView.background = resources.getColor(R.color.Background).toDrawable()
                     timeBarView.stopDrawing()
                     timeBarView.stop()
+                    val sharedPrefEdit: SharedPreferences.Editor = appSettingPrefs.edit()
+                    sharedPrefEdit.putBoolean("NightMode", false)
+
                 }
                 onMenu = !onMenu
             }
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN); //cachage de la barre de status
-        setContentView(layout.activity_main)
-        btnChangeFragment.setOnClickListener(this)
-        timeBarView = findViewById<TimeBarView>(R.id.timeBarView)
-        appSettingPrefs = getSharedPreferences("AppSettingPrefs", 0)
-        val sharedPrefEdit: SharedPreferences.Editor = appSettingPrefs.edit()
-        sharedPrefEdit.putBoolean("NightMode", false)
-
+    fun setFragmentField() {
+        val transaction = manager.beginTransaction()
+        transaction.replace(id.fragment_container, fragmentField)
+        transaction.addToBackStack(null) //conserve le fragment en mémoire
+        transaction.commit()
     }
+     fun setFragmentMenu() {
+         val transaction = manager.beginTransaction()
+         transaction.replace(id.fragment_container, fragmentMenu)
+         transaction.addToBackStack(null) //conserve le fragment en mémoire
+         transaction.commit()
+     }
 
     fun getIsNightModeOn(): Boolean {
         val isNightModeOn: Boolean = appSettingPrefs.getBoolean("NightMode", false)
@@ -100,17 +106,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         if (getIsNightModeOn()) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             sharedPrefEdit.putBoolean("NightMode", false)
-            Toast.makeText(applicationContext,resources.getString(R.string.PopupNightModeOFF),Toast.LENGTH_LONG).show()
+            //Toast.makeText(applicationContext,resources.getString(R.string.PopupNightModeOFF),Toast.LENGTH_LONG).show()
 
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             sharedPrefEdit.putBoolean("NightMode", true)
-            Toast.makeText(applicationContext,resources.getString(R.string.PopupNightModeON),Toast.LENGTH_LONG).show()
+            //Toast.makeText(applicationContext,resources.getString(R.string.PopupNightModeON),Toast.LENGTH_LONG).show()
         }
         sharedPrefEdit.apply()
 
         if (englishOn) { setToEnglish() }
         else { setToFrench() }
+        println(englishOn)
         recreate()
 
 
@@ -127,6 +134,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         recreate()
     }
 
+    fun setToEnglish() {
+        resources.configuration.setLocale(Locale("en"))
+        resources.updateConfiguration(resources.configuration, resources.displayMetrics)
+        Locale.setDefault(Locale("en"))
+    }
+
+    fun setToFrench() {
+        resources.configuration.setLocale(Locale("fr"))
+        resources.updateConfiguration(resources.configuration, resources.displayMetrics)
+        Locale.setDefault(Locale("fr"))
+    }
+
+
+
 
     fun changeMode() {
          if (hardModeOn) {
@@ -141,17 +162,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
          }
     }
 
-    fun setToEnglish() {
-        resources.configuration.setLocale(Locale("en"))
-        resources.updateConfiguration(resources.configuration, resources.displayMetrics)
-        Locale.setDefault(Locale("en"))
-    }
-
-    fun setToFrench() {
-        resources.configuration.setLocale(Locale("fr"))
-        resources.updateConfiguration(resources.configuration, resources.displayMetrics)
-        Locale.setDefault(Locale("fr"))
-    }
 
 }
 
